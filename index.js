16d60057e48623aa964ff57cd1436dc72ee93914
inakilz <inakilz@gmail.com>
2017-09-17 00:34:53 +0200

    añadido ping

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const TelegramBot = require('node-telegram-bot-api');
 const osmosis = require('osmosis');
+const http = require("http");
 
 const token = '445554489:AAGOmrNRNF9-Cn8GCIL3mCh6_GQ_iccqiYI';
 const urlFondo = 'http://www.morningstar.es/es/funds/snapshot/snapshot.aspx?id=F0GBR04QAD';
@@ -7,6 +8,10 @@ const selector = '#overviewQuickstatsDiv table tr:nth-child(2) td.line.text';
 
 const bot = new TelegramBot(token, {polling: true});
 let idInterval = null;
+setInterval(function() {
+  http.get("http://multisalud.herokuapp.com/");
+}, 300000);
+
 
 bot.onText(/\/multisalud/, (msg, match) => {
 	const chatId = msg.chat.id;
