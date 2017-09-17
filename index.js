735cfbf4c32dff664c4140d52b959902dda3466a
inakilz <inakilz@gmail.com>
2017-09-17 23:41:06 +0200

    añadidos mensajes de aceptar/cancelar

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const osmosis = require('osmosis');
 const http = require("http");
 
 const token = '445554489:AAGOmrNRNF9-Cn8GCIL3mCh6_GQ_iccqiYI';
-const urlFondo = 'http://www.morningstar.es/es/funds/snapshot/snapshot.aspx?id=F0GBR04QAD';
 const selector = '#overviewQuickstatsDiv table tr:nth-child(2) td.line.text';
+const urlSearch = 'https://www.google.es/search?q=sites:morningstar.es+fondo+';
+let urlFondo = '';
 
 const bot = new TelegramBot(token, {polling: true});
 let idInterval = null;
@@ -17,47 +18,79 @@ http.createServer(function (req, res) {
   res.end();})
 	.listen(process.env.PORT || 5000);
 
-bot.onText(/\/multisalud/, (msg, match) => {
-	const chatId = msg.chat.id;
-	if (!idInterval) {
-	  //const resp = match[1];
-	  getValorLiquidativo(chatId);
-	  idInterval = setInterval(() => {
-	    getValorLiquidativo(chatId);
-	  }, 1000 * 60 * 60 * 24);
-	  //}, 10000);
-	}
-	else {
-		bot.sendMessage(chatId, 'Proceso ya lanzado');
-	}
-});
-
 bot.onText(/\/stop/, (msg, match) => {
 	const chatId = msg.chat.id;
 	if (idInterval) {
 	  clearInterval(idInterval);
 	  bot.sendMessage(chatId, 'Proceso parado');
 	  idInterval = null;
+	 	urlFondo = null;
 	}
 	else {
 		bot.sendMessage(chatId, 'Proceso ya parado');
 	}
 });
 
-var getValorLiquidativo = function(chatId) {
+bot.onText(/\/search\s+(.*)/, (msg, match) => {
+	const chatId = msg.chat.id;
+	searchFondo(match[1], chatId);
+});
+
+bot.on('message', (msg) => {
+	const chatId = msg.chat.id;
+	if (msg.text == 'Aceptar') {
+	  if (!idInterval) {
+	  	let nombreFondo = msg.reply_to_message.text.replace('Fondo encontrado:\n','');
+		  getValorLiquidativo(chatId, nombreFondo);
+		  idInterval = setInterval(() => {
+		    getValorLiquidativo(chatId, nombreFondo);
+		  }, 1000 * 60 * 60 * 24);
+		  //}, 10000);
+		}
+		else {
+			bot.sendMessage(chatId, 'Proceso ya lanzado');
+		}
+	}
+});
+
+var getValorLiquidativo = function(chatId, nombreFondo) {
 	osmosis
 	  .get(urlFondo)
 	  .find(selector)
 	  .set('valor')
 	  .data(function(data) {
-	    console.log(data);
-	    bot.sendMessage(chatId, 'Valor liquidativo: ' + data.valor);
+	  	console.log(data);
+	    bot.sendMessage(chatId, nombreFondo + '\nValor liquidativo: ' + data.valor);	
 	  })
 	  .log(console.log)
 	  .error(console.log)
 	  .debug(console.log)
 }
 
+var searchFondo = function(fondo, chatId) {
+	osmosis
+	  .get(urlSearch + fondo) 
+    .find('.g:first .r a')
+    .set({
+    	'link': '@href'
+    })
+    .follow('@href')
+    .set({
+      'title': 'title'
+    })
+    .data(function(data) {
+    	console.log(data);
+    	urlFondo = data.link;
+	   	bot.sendMessage(chatId, 'Fondo encontrado:\n' + data.title, {
+				"reply_markup": {
+			    "keyboard": [["Aceptar", "Cancelar"]],
+			  }
+			});
+	  })
+	  .log(console.log)
+	  .error(console.log)
+	  .debug(console.log)
+}
 
 
 
